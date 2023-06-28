0dba58a201e93c1e13ea42773ce643b5e9b2f729
Everton Xavier <evertonxavier@hotmail.com>
2023-06-28 12:25:08 -0300

    Criando a primeira classe de domínio - Categoria

--- src/main/java/com/evertonxavier/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/evertonxavier/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.evertonxavier.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.evertonxavier.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST success";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(1, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
