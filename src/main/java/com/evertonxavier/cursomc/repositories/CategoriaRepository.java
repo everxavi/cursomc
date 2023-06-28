a198b28a8218d0623e993debb99b839677b6deff
Everton Xavier <evertonxavier@hotmail.com>
2023-06-28 17:22:04 -0300

    Criando Repository e Service para o domínio Categoria

---
+++ src/main/java/com/evertonxavier/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.evertonxavier.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.evertonxavier.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
