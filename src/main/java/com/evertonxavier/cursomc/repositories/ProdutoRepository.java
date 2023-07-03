7e127254e8ac663db4d023ebf1f9176ea11dfd93
Everton Xavier <evertonxavier@hotmail.com>
2023-07-03 16:06:46 -0300

    Produto e associação muito para muitos

--- src/main/java/com/evertonxavier/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/evertonxavier/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,14 @@
 package com.evertonxavier.cursomc.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.evertonxavier.cursomc.domain.Categoria;
+import com.evertonxavier.cursomc.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 	
 }
