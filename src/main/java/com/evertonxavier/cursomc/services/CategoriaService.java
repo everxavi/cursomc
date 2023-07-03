4761ff9047dc941d3cdf2b9e9a3c155ea3bd299c
Everton Xavier <evertonxavier@hotmail.com>
2023-07-03 16:51:44 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/evertonxavier/cursomc/services/CategoriaService.java
+++ src/main/java/com/evertonxavier/cursomc/services/CategoriaService.java
@@ -7,14 +7,16 @@ import org.springframework.stereotype.Service;
 
 import com.evertonxavier.cursomc.domain.Categoria;
 import com.evertonxavier.cursomc.repositories.CategoriaRepository;
+import com.evertonxavier.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 	
 	@Autowired
 	private CategoriaRepository repo;
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-	}
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		}
 }
