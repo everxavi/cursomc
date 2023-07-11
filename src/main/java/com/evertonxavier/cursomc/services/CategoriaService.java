30486475ebef902e296086c62728cafb2aa5dcb4
Everton Xavier <evertonxavier@hotmail.com>
2023-07-11 12:29:20 -0300

    Listando todas as Categorias com DTO

--- src/main/java/com/evertonxavier/cursomc/services/CategoriaService.java
+++ src/main/java/com/evertonxavier/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.evertonxavier.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,8 +41,11 @@ public class CategoriaService {
 		catch(DataIntegrityViolationException e){
 			throw new com.evertonxavier.cursomc.services.exceptions.DataIntegrityViolationException(
 					"Não é possível excluir a categoria, pois existem produtos com ela relacionados");
-		}
-		
+		}		
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 	
 }
